ba4d07f005c0d52259c951fc3ee4f021ac01d4a9
zeeshan-saleem510 <zeeshan.saleem510@gmail.com>
2025-07-14 12:10:06 +0100

    resource group mans

--- Todoapp_parent/main.tf
+++ Todoapp_parent/main.tf
@@ -7,8 +7,8 @@ module "resource_group" {
 
 module "resource_group" {
   source                  = "../modules/azurerm_resource_group"
-  resource_group_name     = "mq_rg"
-  resource_group_location = "centralindia"
+  resource_group_name     = "mans_rg"
+  resource_group_location = "eastus2"
 }
 
 module "virtual_network" {
